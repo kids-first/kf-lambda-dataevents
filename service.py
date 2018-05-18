ddb13f36d1327cc4ee333ee5af902ae214fe63c4
Dan Kolbman <dankolbman@gmail.com>
2018-05-17 21:23:08 -0400

    :sparkles: Add full functionality

--- service.py
+++ service.py
@@ -1,23 +1,22 @@
 import os
+import gzip
+import io
 import boto3
 import json
+import time
 from botocore.vendored import requests
 
 
-s3 = boto3.client("s3", region_name='us-east-1')
 
-
-SQS_URL = os.environ.get('SQS_URL', None)
-BUCKET = os.environ.get('BUCKET', None)
-SLACK_TOKEN = os.environ.get('SLACK_SECRET', None)
-SLACK_CHANNELS = os.environ.get('SLACK_CHANNEL', '').split(',')
-SLACK_CHANNELS = [c.replace('#','').replace('@','') for c in SLACK_CHANNELS]
+WAIT_TIME = 6
 
 
 def handler(event, context):
     """
     Iterate over an sqs queue and write events to a queue
     """
+    BUCKET = os.environ.get('BUCKET', None)
+    SQS_URL = os.environ.get('SQS_URL', None)
     if not BUCKET or not SQS_URL:
         return 'BUCKET and SQS_URL must be provided'
 
@@ -25,27 +24,110 @@ def handler(event, context):
     queue = sqs.Queue(SQS_URL)
 
     # If this isn't a re-invocation
-    if 'key' not in event:
+    invoked = event.get('invoked', 1)
+    if invoked == 1:
         # Send slack notif
         attachments = [
-            { "fallback": "I'm about to run daily data event log compaction!",
-              "text": "I'm about to run daily data event log compaction!",
+            { "fallback": "I'm about to run daily data event log compaction!\nHere we gooooo!!!!",
+              "text": "I'm about to run daily data event log compaction!\nHere we gooooo!!!!",
               "color": "#005e99"
             }
         ]
         send_slack(attachments=attachments)
 
-    for message in queue.receive_messages(MaxNumberOfMessages=10, WaitTimeSeconds=4):
-        # process message body
-        body = json.loads(message.body)
-        print('check')
-        print(body['MessageId'], json.loads(body['Message'])['data']['results']['kf_id'])
+    messages = {}
+    empty_batches = 0
+
+    batch = [None]
+    while empty_batches < 3 and len(batch) > 0:
+        # Running out of time
+        if context.get_remaining_time_in_millis() < 5000:
+            # Upload, then re-invoke to continue processing
+            save_messages(messages)
+            attachments = [
+                { "fallback": "There's still events to process, but no time left! Re-invoking...",
+                  "text": "There's still events to process, but no time left! Re-invoking...",
+                  "fields": [
+                      {
+                          "title": "Events Saved",
+                          "value": len(messages),
+                          "short": True
+                      },
+                      {
+                          "title": "Function Calls (so far)",
+                          "value": invoked,
+                          "short": True
+                      }
+                  ],
+                  "color": "warning"
+                }
+            ]
+            send_slack(attachments=attachments)
+            # Re-invoke
+            lam = boto3.client('lambda', region_name='us-east-1')
+            response = lam.invoke(
+                FunctionName=context.function_name,
+                InvocationType='Event',
+                Payload=str.encode(json.dumps({'invoked': invoked})),
+            )
+            break
+        # Get the next batch of messages
+        batch = queue.receive_messages(MaxNumberOfMessages=10,
+                                       WaitTimeSeconds=1)
+
+        if len(batch) == 0:
+            empty_batches += 1
+            continue
+
+        # Iterate messages
+        for message in batch:
+            m = json.loads(message.body)
+            messages[message.message_id] = m['Message'].replace("'", '"').encode()
+            resp = message.delete()
+    else:
+        save_messages(messages)
+        attachments = [
+            { "fallback": "Finished storing the daily logs",
+              "text": "Finished storing the daily logs",
+              "fields": [
+                  {
+                      "title": "Events Saved",
+                      "value": len(messages),
+                      "short": True
+                  },
+                  {
+                      "title": "Function Calls",
+                      "value": invoked,
+                      "short": True
+                  }
+              ],
+              "color": "good"
+            }
+        ]
+        send_slack(attachments=attachments)
+
+
+def save_messages(messages):
+    """
+    Compress and upload messages
+    """
+    BUCKET = os.environ.get('BUCKET', None)
+    s3 = boto3.client("s3", region_name='us-east-1')
+
+    out = b'\n'.join(messages.values())
+    # Save to a file with current timestamp
+    resp = s3.put_object(Body=gzip.compress(out),
+                         Bucket=BUCKET,             
+                         Key='daily/{}.gz'.format(int(time.time())))
 
 
 def send_slack(msg=None, attachments=None):
     """
     Sends a slack notification
     """
+    SLACK_TOKEN = os.environ.get('SLACK_SECRET', None)
+    SLACK_CHANNELS = os.environ.get('SLACK_CHANNEL', '').split(',')
+    SLACK_CHANNELS = [c.replace('#','').replace('@','') for c in SLACK_CHANNELS]
     if SLACK_TOKEN is not None:
         for channel in SLACK_CHANNELS:
             message = {
--- tests/test_service.py
+++ tests/test_service.py
@@ -1,8 +1,151 @@
+import io
+import os
+import boto3
+import gzip
+import json
 import pytest
+from moto import mock_s3, mock_sqs, mock_lambda
+from mock import MagicMock, patch
 import service
 
 BUCKET = 'kf-events-us-east-1-dev-dataservice-dataevents'
+KEY = 'daily/log.txt.gz'
+QUEUE = 'kf-dataevents-sqs'
 
 
-def test():
-    pass
+class Context:
+    def __init__(self, remaining=20000):
+        self.remaining = remaining
+        self.function_name = 'kf-dataevents'
+
+    def get_remaining_time_in_millis(self):
+        self.remaining -= 500
+        return self.remaining
+
+@pytest.fixture
+def obj():
+    @mock_s3
+    def with_obj():
+        """ Create a harmonized file and its source file """
+        s3 = boto3.client('s3', region_name='us-east-1')
+        b = s3.create_bucket(Bucket=BUCKET)
+        return s3
+    return with_obj
+
+
+@pytest.fixture()
+def queue():
+    @mock_sqs
+    def with_sqs():
+        client = boto3.client('sqs', region_name='us-east-1')
+        response = client.create_queue(QueueName=QUEUE)
+        url = response['QueueUrl']
+        os.environ['SQS_URL'] = url
+        return url
+    return with_sqs
+
+
+@pytest.fixture
+def req():
+    req_patch = patch('service.requests')
+    req_mock = req_patch.start()
+    yield req_mock
+    req_mock.stop()
+
+
+@pytest.fixture
+def env():
+    os.environ['BUCKET'] = BUCKET
+    os.environ['SLACK_SECRET'] = 'abc'
+    os.environ['SLACK_CHANNELS'] = 'chat'
+
+
+@mock_sqs
+@pytest.fixture
+def messages(queue):
+    queue_url = queue()
+
+    msg = []
+    with open('tests/messages.json') as f:
+        for l in f:
+            msg.append(l)
+
+    for m in msg:
+        sqs = boto3.client('sqs', region_name='us-east-1')
+        # Send test message to queue
+        response = sqs.send_message(
+            QueueUrl=queue_url,
+            MessageBody=m
+        )
+    return msg
+
+
+@mock_lambda
+@mock_s3
+def test_upload(obj, messages, req, env):
+    """
+    Test that compressed log file is created
+    """
+    s3 = obj()
+    
+    service.handler({}, Context(6000))
+    
+    assert len(s3.list_objects(Bucket=BUCKET)['Contents']) == 1
+
+    stream = io.BytesIO()
+    KEY = s3.list_objects(Bucket=BUCKET)['Contents'][0]['Key']
+    ob = s3.get_object(Bucket=BUCKET, Key=KEY)
+    dat = gzip.decompress(ob['Body'].read()).decode()
+
+    for i, l in enumerate(dat.split('\n')):
+        assert json.loads(messages[i])['Message'].replace("'", '"') == l
+        message = json.loads(l)
+
+        assert 'path' in message
+        assert 'method' in message
+        assert 'api_version' in message
+        assert 'api_commit' in message
+        assert 'data' in message
+
+    assert req.post.call_count == 2
+
+
+@mock_lambda
+@mock_s3
+def test_delete(obj, messages, queue, req, env):
+    """
+    Test that messages are deleted after being written
+    """
+    s3 = obj()
+    queue_url = queue()
+    
+    service.handler({}, Context(6000))
+    sqs = boto3.resource('sqs', region_name='us-east-1')
+    queue = sqs.Queue(queue_url)
+    
+    batch = queue.receive_messages(MaxNumberOfMessages=10,
+                                   WaitTimeSeconds=0)
+    assert len(batch) == 0
+    assert req.post.call_count == 2
+
+
+@mock_lambda
+@mock_s3
+def test_reinvoke(obj, messages, queue, req, env):
+    """
+    Test that messages are deleted after being written
+    """
+    s3 = obj()
+    queue_url = queue()
+    patch_lam = patch('boto3.client')
+    mock_lam = patch_lam.start()
+    
+    service.handler({}, Context(200))
+    sqs = boto3.resource('sqs', region_name='us-east-1')
+    queue = sqs.Queue(queue_url)
+
+    assert mock_lam().invoke.call_count == 1
+    assert mock_lam().put_object.call_count == 1
+    assert req.post.call_count == 2
+
+    mock_lam.stop()
